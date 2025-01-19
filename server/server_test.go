4663ff7537666f369ccb18ad3ffcd5c7a79fd835
Peter Strøiman <peter@stroiman.com>
2025-01-19 15:50:54 +0100

    Install go-dom

---
+++ server/server_test.go
@@ -0,0 +1,22 @@
+package server_test
+
+import (
+	"fmt"
+	"harmony/server"
+	"testing"
+
+	"github.com/stroiman/go-dom/browser"
+)
+
+func TestCanServe(t *testing.T) {
+	b := browser.NewBrowserFromHandler(server.New())
+	w, err := b.Open("/")
+	if err != nil {
+		t.Fatal(err)
+	}
+	h1, err := w.Document().Body().QuerySelector("h1")
+	if err != nil {
+		t.Fatal(err)
+	}
+	fmt.Print(h1.OuterHTML())
+}
