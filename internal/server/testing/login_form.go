7a3c5186d2512c23ca9830f5eafdff5976fa8987
Peter Strøiman <peter@stroiman.com>
2025-02-07 07:55:06 +0100

    Extract LoginForm test helper

---
+++ internal/server/testing/login_form.go
@@ -0,0 +1,29 @@
+package testing
+
+import (
+	ariarole "harmony/internal/testing/aria-role"
+	"harmony/internal/testing/shaman"
+	. "harmony/internal/testing/shaman/predicates"
+
+	"github.com/gost-dom/browser/dom"
+)
+
+type LoginForm struct {
+	shaman.Scope
+}
+
+func NewLoginForm(s shaman.Scope) LoginForm {
+	return LoginForm{s.Subscope(ByRole(ariarole.Form))}
+}
+
+func (f LoginForm) Email() dom.Element {
+	return f.Get(ByRole(ariarole.Textbox), ByName("Email"))
+}
+
+func (f LoginForm) Password() dom.Element {
+	return f.Get(ByRole(ariarole.PasswordText), ByName("Password"))
+}
+
+func (f LoginForm) SubmitBtn() dom.Element {
+	return f.Get(ByRole(ariarole.Button), ByName("Sign in"))
+}
