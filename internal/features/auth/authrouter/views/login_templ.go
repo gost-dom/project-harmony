378a33921a14e997cb9b89599b5ce47d48625484
Peter Strøiman <peter@stroiman.com>
2025-03-18 17:09:18 +0100

    Remove unnecessary file prefix

--- internal/features/auth/authrouter/views/login_templ.go
+++ internal/features/auth/authrouter/views/login_templ.go
@@ -127,7 +127,7 @@ func LoginForm(redirectUrl string, formData LoginFormData) templ.Component {
 		var templ_7745c5c3_Var4 string
 		templ_7745c5c3_Var4, templ_7745c5c3_Err = templ.JoinStringErrs(redirectUrl)
 		if templ_7745c5c3_Err != nil {
-			return templ.Error{Err: templ_7745c5c3_Err, FileName: `internal/features/auth/authrouter/views/auth_login.templ`, Line: 64, Col: 21}
+			return templ.Error{Err: templ_7745c5c3_Err, FileName: `internal/features/auth/authrouter/views/login.templ`, Line: 64, Col: 21}
 		}
 		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var4))
 		if templ_7745c5c3_Err != nil {
