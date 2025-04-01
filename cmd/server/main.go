9b81bc17b8467b97cc2087b0285014cb7a104c87
Peter Strøiman <peter@stroiman.com>
2025-04-01 22:24:20 +0200

    Choose a different default dev server port
    Port 8081 is commonly used for other tools as well.

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,13 +5,6 @@ import (
 	"net/http"
 )
 
-func noCache(h http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Add("cache-control", "no-cache")
-		h.ServeHTTP(w, r)
-	})
-}
-
 func main() {
-	http.ListenAndServe("0.0.0.0:8081", ioc.Server())
+	http.ListenAndServe("0.0.0.0:9999", ioc.Server())
 }
