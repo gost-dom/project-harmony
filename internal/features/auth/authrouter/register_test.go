bdc838cd13911d8d986bc6ef03238346c3f746b4
Peter Strøiman <peter@stroiman.com>
2025-04-02 12:03:19 +0200

    Skeleton registration test

--- internal/features/auth/authrouter/register_test.go
+++ internal/features/auth/authrouter/register_test.go
@@ -8,6 +8,7 @@ import (
 	. "harmony/internal/testing/shaman/predicates"
 	"testing"
 
+	"github.com/gost-dom/browser/html"
 	matchers "github.com/gost-dom/browser/testing/gomega-matchers"
 	"github.com/stretchr/testify/suite"
 )
@@ -23,7 +24,7 @@ func TestRegister(t *testing.T) {
 
 func (s *RegisterTestSuite) SetupTest() {
 	s.BrowserSuite.SetupTest()
-	s.OpenWindow("/auth/register")
+	s.OpenWindow("https://example.com/auth/register")
 }
 
 func (s *RegisterTestSuite) TestSubmitValidForm() {
@@ -31,10 +32,22 @@ func (s *RegisterTestSuite) TestSubmitValidForm() {
 
 	form := RegisterForm{s.Subscope(predicates.ByRole(ariarole.Form))}
 	form.FullName().Write("John Smith")
+	form.Email().Write("john.smith@example.com")
+	form.Submit().Click()
+
+	s.Expect(s.Scope.Get(ByH1)).To(matchers.HaveTextContent("Validate Email"))
 }
 
 type RegisterForm struct{ shaman.Scope }
 
 func (f RegisterForm) FullName() shaman.TextboxRole {
 	return f.Textbox(ByName("Full name"))
+}
+
+func (f RegisterForm) Email() shaman.TextboxRole {
+	return f.Textbox(ByName("Email"))
+}
+
+func (f RegisterForm) Submit() html.HTMLElement {
+	return f.Get(shaman.ByRole(ariarole.Button))
 }
