25ce9758bf319a87fe17021ac30e13a11d9b6223
Peter Strøiman <peter@stroiman.com>
2025-04-12 16:03:45 +0200

    Improce code doc

--- internal/features/auth/authdomain/account.go
+++ internal/features/auth/authdomain/account.go
@@ -147,8 +147,8 @@ func (a *Account) Authenticated() (AuthenticatedAccount, error) {
 // authentication flow. Code that needs to check who is performing an operation
 // can depend on this type.
 //
-// At the moment this type merely indicatest that an authentication chack has
+// At the moment this type merely indicates that an authentication check has
 // succeeded. But it could hold information regarding which kind of
 // authentication mechanism was used, e.g., password, passkey. Was 2FA used,
-// etc. It this a revisit from a user with "remember me" enabled.
+// etc. Or the a returning user with "remember me" enabled.
 type AuthenticatedAccount struct{ *Account }
--- internal/testing/domaintest/authdomain.go
+++ internal/testing/domaintest/authdomain.go
@@ -2,6 +2,16 @@ package domaintest
 
 import "harmony/internal/features/auth/authdomain"
 
+// InitAccount creates and returns a valid minimal Account for test scenarios
+// that requires a valid account, but details are irrelevant.
+func InitAccount() authdomain.Account {
+	return authdomain.Account{}
+}
+
+// InitAuthenticatedAccount creates and returns an AuthenticatedAccount with a
+// minimal account for use in test scenarios where an authenticated account is
+// required, but the specific user details are irrelevant.
 func InitAuthenticatedAccount() authdomain.AuthenticatedAccount {
-	return authdomain.AuthenticatedAccount{Account: &authdomain.Account{}}
+	acc := InitAccount()
+	return authdomain.AuthenticatedAccount{Account: &acc}
 }
