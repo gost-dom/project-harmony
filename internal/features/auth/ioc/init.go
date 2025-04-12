e65a090ffed571649f00450b8952e40fbb9881d3
Peter Strøiman <peter@stroiman.com>
2025-04-13 00:01:15 +0200

    Remove useless dummy type

--- internal/features/auth/ioc/init.go
+++ internal/features/auth/ioc/init.go
@@ -1,7 +1,6 @@
 package ioc
 
 import (
-	"context"
 	"harmony/internal/features/auth"
 	"harmony/internal/features/auth/authrouter"
 
@@ -11,11 +10,4 @@ import (
 func Install[T any](graph *surgeon.Graph[T]) *surgeon.Graph[T] {
 	graph = surgeon.Replace[authrouter.Registrator](graph, &auth.Registrator{})
 	return graph
-	//surgeon.Replace[auth.AccountRepository](graph, dummyAccRepo{})
-}
-
-type dummyAccRepo struct{}
-
-func (r dummyAccRepo) Insert(context.Context, auth.AccountUseCaseResult) error {
-	return nil
 }
