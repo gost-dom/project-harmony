9afd096d1dec1b98306eb6952395ea5a505199dd
Peter Strøiman <peter@stroiman.com>
2025-04-12 08:18:30 +0200

    Allow tests to control cookies

--- internal/testing/servertest/browser_suite.go
+++ internal/testing/servertest/browser_suite.go
@@ -5,12 +5,26 @@ import (
 	"harmony/internal/server"
 	"harmony/internal/testing/htest"
 	"harmony/internal/testing/shaman"
+	"net/http/cookiejar"
 	"time"
 
 	"github.com/gost-dom/browser"
 	"github.com/gost-dom/browser/html"
 	"github.com/gost-dom/surgeon"
 )
+
+type CookieJar struct {
+	*cookiejar.Jar
+}
+
+func (j *CookieJar) Clear() {
+	j.Jar, _ = cookiejar.New(nil)
+}
+
+func NewCookieJar() *CookieJar {
+	jar, _ := cookiejar.New(nil)
+	return &CookieJar{jar}
+}
 
 // BrowserSuite is an extension to [suite.Suite] that adds common behaviour for
 // interacting with the website using a browser.
@@ -24,7 +38,9 @@ import (
 type BrowserSuite struct {
 	htest.GomegaSuite
 	shaman.Scope
+	CookieJar *CookieJar
 	Graph     *surgeon.Graph[*server.Server]
+	Browser   *browser.Browser
 	Win       html.Window
 	Ctx       context.Context
 	CancelCtx context.CancelFunc
@@ -41,7 +57,9 @@ func (s *BrowserSuite) OpenWindow(path string) html.Window {
 		panic("BrowserSuite: This suite does not support opening multiple windows pr. test case")
 	}
 	serv := s.Graph.Instance()
-	b := browser.NewBrowserFromHandler(serv)
+	s.Browser = browser.New(browser.WithHandler(serv))
+	s.CookieJar = NewCookieJar()
+	s.Browser.Client.Jar = s.CookieJar
 
 	// Opening "about:blank" is a bit of a hack to allow adding the event sync
 	// _before_ loading an HTMX page; as by the time `Open` or `Navigate`
@@ -54,7 +72,7 @@ func (s *BrowserSuite) OpenWindow(path string) html.Window {
 	// A future version of Gost will support adding the event handler _before_
 	// navigating, to avoid the overhead creating a new script context; although
 	// it should be minimal.
-	win, err := b.Open("about:blank")
+	win, err := s.Browser.Open("about:blank")
 	s.Assert().NoError(err)
 	err = win.Navigate(path)
 	s.Assert().NoError(err)
