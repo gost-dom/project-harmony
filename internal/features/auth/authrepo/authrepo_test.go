b4eb00616c9b0e3785b98470ea03a4f3593aeb6c
Peter Strøiman <peter@stroiman.com>
2025-04-13 08:23:31 +0200

    New couch connection bootstraps the database

--- internal/features/auth/authrepo/authrepo_test.go
+++ internal/features/auth/authrepo/authrepo_test.go
@@ -13,6 +13,18 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+var ErrCommunication = CommunicationError{}
+
+type CommunicationError struct{ error }
+
+func (CommunicationError) Is(err error) bool {
+	_, is := err.(CommunicationError)
+	return is
+}
+func (e CommunicationError) Error() string {
+	return fmt.Sprintf("couchdb: communication error: %v", e.error)
+}
+
 type Doc struct {
 	Foo string
 }
@@ -22,10 +34,42 @@ type CouchConnection struct {
 	dbName string
 }
 
-func NewCouchConnection(couchURL string) (CouchConnection, error) {
-	url, err := url.Parse(couchURL)
-	dbName := url.Path
-	return CouchConnection{couchURL, dbName}, err
+// Bootstrap creates the database, as well as updates any design documents, such
+// as views. Panics on unrecoverable errors, e.g., an invalid configuration.
+func (c CouchConnection) Bootstrap() error {
+	req, err := http.NewRequest("PUT", c.url, nil)
+	if err != nil {
+		// This is an unrecoverable error. The system configuration is wrong.
+		// NewRequest will return an error is one of the arguments are invalid.
+		// in this context that would mean that the URL is invalid.
+		panic("couchdb: invalid configuration: url invalid")
+	}
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return CommunicationError{err}
+	}
+	switch resp.StatusCode {
+	case 201, 202, 412:
+		return nil
+	case 400:
+		// This is an unrecoverable error.
+		panic("couchdb: invalid configuration: illegal database name")
+	case 401:
+		// This is an unrecoverable error.
+		panic("couchdb: invalid configuration: bad credentials")
+	default:
+		panic("couchdb: unable to bootstrap database")
+	}
+}
+
+func NewCouchConnection(couchURL string) (conn CouchConnection, err error) {
+	var url *url.URL
+	url, err = url.Parse(couchURL)
+	conn = CouchConnection{couchURL, url.Path}
+	if err == nil {
+		err = conn.Bootstrap()
+	}
+	return
 }
 
 func (c CouchConnection) getDocId(location *url.URL) string {
@@ -44,7 +88,7 @@ func (c CouchConnection) Insert(doc any) (id string, err error) {
 		return
 	}
 	if resp.StatusCode != 201 {
-		err = fmt.Errorf("CouchConnection.Insert: Unexpected status code: %d", resp.StatusCode)
+		err = fmt.Errorf("couch: bad status code: %d", resp.StatusCode)
 		return
 	}
 	loc, err := resp.Location()
@@ -80,4 +124,12 @@ func TestDatabaseRoundtrip(t *testing.T) {
 
 	// Verify they are equal
 	assert.Equal(t, "Bar", actual.Foo)
+}
+
+func TestDatabaseBootstrap(t *testing.T) {
+	if testing.Short() {
+		return
+	}
+	_, err := NewCouchConnection("http://invalid.localhost/")
+	assert.ErrorIs(t, err, ErrCommunication)
 }
