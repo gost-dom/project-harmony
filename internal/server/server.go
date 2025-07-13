e25cdff1a4d37a78c247df7effa2b72af8f9b648
Peter Strøiman <peter@stroiman.com>
2025-07-13 14:43:57 +0200

    Add an Unwrap() function to StatusRecorder

--- internal/server/server.go
+++ internal/server/server.go
@@ -19,6 +19,8 @@ import (
 	gonanoid "github.com/matoous/go-nanoid/v2"
 )
 
+// StatusRecorder embeds an [http.ResponseWriter] which remembers the status
+// code being generated, allowing client to retroactively query the status code.
 type StatusRecorder struct {
 	http.ResponseWriter
 	code int
@@ -28,6 +30,12 @@ func (r *StatusRecorder) WriteHeader(code int) {
 	r.ResponseWriter.WriteHeader(code)
 	r.code = code
 }
+
+// Unwrap implements the unexported rwUnwrapper interface. This is necessary for
+// [http.ResponseController] to get the underlying ResponseWriter, e.g. to
+// query for cabilities like [http.Flusher].
+func (r *StatusRecorder) Unwrap() http.ResponseWriter { return r.ResponseWriter }
+
 func (r *StatusRecorder) Code() int {
 	if r.code == 0 {
 		return 200
